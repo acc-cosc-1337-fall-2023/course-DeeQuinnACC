05d2bf3740f3374c4e2466ca142f6191b69c0df6
DeeQuinnACC <142953458+DeeQuinnACC@users.noreply.github.com>
2023-09-01 00:19:15 +0000

    double functions added, comparison inconsistencies

--- test/examples/01_data_types/01_data_types_tests.cpp
+++ test/examples/01_data_types/01_data_types_tests.cpp
@@ -1,23 +1,31 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "int.h"
+#include "decimals.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
 /*test case echo_variable */
-TEST_CASE("Test echo_variable Function", "functon should return the same value that was passed in") {
+TEST_CASE("Test echo_variable Function", "functon should return the same value that was passed in") 
+{
 	REQUIRE(5 == echo_variable(5));
 	REQUIRE(10 == echo_variable(10));
 	REQUIRE(100 == echo_variable(100));
 }
 /*test case add_to_double_1 with 0 as parameter*/
-
+TEST_CASE("Test add_to_double_1 Function", "function should return the value passed in plus 0.9")
+{
+	//REQUIRE(0.9 == add_to_double_1(0.0));
+}
 /*test case add_to_double_1 with 1 as parameter*/
 
 /*test case add_to_double_2 with 0 as parameter*/
-
+TEST_CASE("Test add_to_double_2 Function", "function should return the value passed in plus 1.5")
+{
+	REQUIRE(1.5 == add_to_double_2(0.0));
+}
 /*test case add_to_double_2 with 1 as parameter*/
 
 /*test case char test ASCII values */
