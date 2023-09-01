cbc3bb364623d36a16e0aba0f3ca497dc5e784c9
DeeQuinnACC <142953458+DeeQuinnACC@users.noreply.github.com>
2023-09-01 00:24:40 +0000

    intro to doubles: main

--- src/examples/01_data_types/02_decimals/main.cpp
+++ src/examples/01_data_types/02_decimals/main.cpp
@@ -1,5 +1,18 @@
+#include<iostream>
+#include "decimals.h"
+
+using std::cout;
+using std::cin;
+
 int main()
 {
+    double value; //Create variable
+    cout<<"Enter a value: ";
+    cin>>value;
+
+    double result = add_to_double_1(value);
+
+    cout<<"\nResult: "<<result<<"\n";
 
     return 0;
 }
