e5554fb0994387b0b456921608e410b2df580327
DeeQuinnACC <142953458+DeeQuinnACC@users.noreply.github.com>
2023-09-01 00:59:06 +0000

    intro to char

--- src/examples/01_data_types/04_char/main.cpp
+++ src/examples/01_data_types/04_char/main.cpp
@@ -1,5 +1,15 @@
+#include<iostream>
+#include "char.h"
+
+using std::cout;
+
 int main()
 {
+    char letter = 'A';
+
+    auto ascii_value = get_char_ascii_value(letter);
+
+    cout<<"Ascii Value for "<<letter<<" is "<<ascii_value<<"\n";
 
     return 0;
 }
--- test/examples/01_data_types/01_data_types_tests.cpp
+++ test/examples/01_data_types/01_data_types_tests.cpp
@@ -2,6 +2,7 @@
 #include "catch.hpp"
 #include "int.h"
 #include "decimals.h"
+#include "char.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -29,6 +30,11 @@ TEST_CASE("Test add_to_double_2 Function", "function should return the value pas
 /*test case add_to_double_2 with 1 as parameter*/
 
 /*test case char test ASCII values */
-
+TEST_CASE("Test get_char_ascii_value", "function should return the ascii value of the character inputed")
+{
+	REQUIRE(65 == get_char_ascii_value('A'));
+	REQUIRE(68 == get_char_ascii_value('D'));
+	REQUIRE(32 == get_char_ascii_value(' '));
+}
 /*test case string test ASCII values Joe characters are equal to 74, 111, and 101*/
 
