45e9b59ceec55c618aef8799c086a5e2d622e4db
DeeQuinnACC <142953458+DeeQuinnACC@users.noreply.github.com>
2023-09-01 00:32:06 +0000

    intro to auto keyword

--- src/examples/01_data_types/03_auto/main.cpp
+++ src/examples/01_data_types/03_auto/main.cpp
@@ -1,5 +1,11 @@
+#include<iostream>
+
+using std::cout;
+
 int main()
 {
-
+    //Auto will attempt to automatically apply a data type to a variable
+    //according to the value inputed.
+    auto num = 5;
     return 0;
 }
