9b635ca6f1c38216cc63de40ceffa01fdcf70871
DeeQuinnACC <142953458+DeeQuinnACC@users.noreply.github.com>
2023-09-13 00:56:42 +0000

    switch fucntion complete

--- test/examples/03_conditionals/03_conditionals_tests.cpp
+++ test/examples/03_conditionals/03_conditionals_tests.cpp
@@ -1,10 +1,8 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include <iostream>
 #include "if.h"
 #include "if_else_if.h"
-
-using std::string;
+#include "switch.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -24,4 +22,13 @@ TEST_CASE("Verify get_generation function")
 	REQUIRE(get_generation(1960) == "Baby Boomer");
 	REQUIRE(get_generation(1940) == "Silent Generation");
 	REQUIRE(get_generation(1111) == "Invalid Year");
+}
+
+TEST_CASE("Verify menu function")
+{
+	REQUIRE(menu(1) == "Option 1");
+	REQUIRE(menu(2) == "Option 2");
+	REQUIRE(menu(3) == "Option 3");
+	REQUIRE(menu(4) == "Option 4");
+	REQUIRE(menu(0) == "Invalid Option");
 }
