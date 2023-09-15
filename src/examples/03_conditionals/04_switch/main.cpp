53a923a163b50a326ee94c9d41be49154cfefbc3
DeeQuinnACC <142953458+DeeQuinnACC@users.noreply.github.com>
2023-09-15 00:01:10 +0000

    switch main example complete

--- src/examples/03_conditionals/04_switch/main.cpp
+++ src/examples/03_conditionals/04_switch/main.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
+#include "switch.h"
+
+using std::cout;
+using std::cin;
 
 int main() 
 {
-	
+	int option = -1;
+	cout<<"Enter a number: ";
+	cin>>option;
+
+	auto choice = menu(option);
+
+	cout<<"You choose "<<choice<<".\n";
 
 	return 0;
 }
