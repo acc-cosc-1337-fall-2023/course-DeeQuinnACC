9fcc3a07bdde4df4f9e06928936a76a32422ee67
DeeQuinnACC <142953458+DeeQuinnACC@users.noreply.github.com>
2023-09-16 19:56:09 +0000

    homework 3 complete

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,59 @@
 //write include statement for decisions header
-
+#include "decisions.h"
 
 //Write code for function(s) code here
+char get_letter_grade_using_if(int grade)
+{
+    char letterGrade;
+
+    if(grade >= 90)
+    {
+        letterGrade = 'A';
+    }
+    else if(grade >= 80)
+    {
+        letterGrade = 'B';
+    }
+    else if(grade >= 70)
+    {
+        letterGrade = 'C';
+    }
+    else if(grade >= 60)
+    {
+        letterGrade = 'D';
+    }
+    else
+    {
+        letterGrade = 'F';
+    }
+
+    return letterGrade;
+}
+
+char get_letter_grade_using_switch(int grade)
+{
+    char letterGrade;
+
+    switch(grade)
+    {
+        case 0 ... 59:
+            letterGrade = 'F';
+            break;
+        case 60 ... 69:
+            letterGrade = 'D';
+            break;
+        case 70 ... 79:
+            letterGrade = 'C';
+            break;
+        case 80 ... 89:
+            letterGrade = 'B';
+            break;
+        case 90 ... 100:
+            letterGrade = 'A';
+            break;
+        default:
+            letterGrade = 'A';
+            break;
+    }
+    return letterGrade;
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,62 @@
 //write include statements
+#include <iostream>
+#include "decisions.h"
+
+using std::cout;
+using std::cin;
 
 int main() 
 {
+	//Initialize variables
+	int grade;
+	int choice;
+	char letterGrade;
+
+	//Main menu
+	cout<<"\tMain Menu\n";
+	cout<<"1 - Letter grade using if\n";
+	cout<<"2 - Letter grade using switch\n";
+	cout<<"3 - Exit\n";
+
+	//Get choice
+	cout<<"\nPlease enter a number listed above: ";
+	cin>>choice;
+	//Check that the user didn't exit the program
+	if(choice != 3)
+	{
+		//Check if the choice is valid
+		if(choice < 1 || choice > 3)
+		{
+			cout<<"Invalid choice, please enter a choice listed above.\n";
+			return 0;
+		}
+
+		//Get grade
+		cout<<"\nPlease enter your grade: ";
+		cin>>grade;
+
+		//Check if grade is valid
+		if(grade < 0 || grade > 100)
+		{
+			cout<<"The entered grade is invalid, please try again.\n";
+			return 0;
+		}
+
+		//Get letter grade
+		if(choice == 1)
+		{
+			letterGrade = get_letter_grade_using_if(grade);
+		}
+		else
+		{
+			letterGrade = get_letter_grade_using_switch(grade);
+		}
+
+		//Output grade
+		cout<<"Your letter grade is "<<letterGrade<<".";
+	}
+
+	//Exit program
+	cout<<"\nExiting program...\n";
 	return 0;
 }
