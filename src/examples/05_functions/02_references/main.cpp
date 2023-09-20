cf601bc699ae7ac2e67f8fd976bb381025079ca4
DeeQuinnACC <142953458+DeeQuinnACC@users.noreply.github.com>
2023-09-20 00:23:06 +0000

    learning references

--- src/examples/05_functions/02_references/main.cpp
+++ src/examples/05_functions/02_references/main.cpp
@@ -1,4 +1,21 @@
+#include<iostream>
+
+using std::cout;
+
 int main()
 {
+    //num_ref is a reference to num
+    int num = 5;
+    int& num_ref = num;
+
+    //It is at the same location
+    cout<<num_ref<<" is at "<<&num_ref;
+    cout<<"\nCompare "<<&num;
+
+    //It will update num all the same
+    num_ref = 10;
+
+    cout<<"\n"<<num_ref<<", "<<num;
+
     return 0;
 }
