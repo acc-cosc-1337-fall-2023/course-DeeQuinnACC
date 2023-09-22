690c9f0755f27db65222999e7bb15dc5749550a6
DeeQuinnACC <142953458+DeeQuinnACC@users.noreply.github.com>
2023-09-22 00:00:43 +0000

    comment in val_ref.cpp

--- src/examples/05_functions/03_val_ref_params/val_ref.cpp
+++ src/examples/05_functions/03_val_ref_params/val_ref.cpp
@@ -10,3 +10,8 @@ void const_ref_param(const int &num)
     //This next line won't compile
     //num = 20;
 }
+
+
+//References are best used for lists and large amounts of data
+//Since otherwise it would copy large amoutns of data every
+//time you call the function
