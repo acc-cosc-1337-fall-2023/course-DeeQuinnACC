ea3acb5802ad2740ab46d50c734cfd080379eba9
DeeQuinnACC <142953458+DeeQuinnACC@users.noreply.github.com>
2023-09-27 00:37:39 +0000

    string playaround

--- src/examples/06_strings/01_create/main.cpp
+++ src/examples/06_strings/01_create/main.cpp
@@ -6,8 +6,22 @@ using std::string;
 
 int main()
 {
-    string lang = "C++++++++++++++";
+    string lang = "abcdefghijklmno";
 
     cout<<lang.capacity()<<"\n";
+    cout<<&lang<<"\n";
+    for(int i = 0; i < lang.size(); i++)
+    {
+        cout<<static_cast<void*>(&lang[i])<<"\n";
+    }
+
+    lang.append("p");
+
+    cout<<"\n"<<&lang<<"\n";
+    for(int i = 0; i < lang.size(); i++)
+    {
+        cout<<static_cast<void*>(&lang[i])<<"\n";
+    }
+
     return 0;
 }
