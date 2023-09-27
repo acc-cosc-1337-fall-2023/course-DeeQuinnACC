371973c4e1f8babcacd2af414b6113c13ba42475
DeeQuinnACC <142953458+DeeQuinnACC@users.noreply.github.com>
2023-09-27 00:58:09 +0000

    notes for creating strings

--- src/examples/06_strings/01_create/main.cpp
+++ src/examples/06_strings/01_create/main.cpp
@@ -23,5 +23,24 @@ int main()
         cout<<static_cast<void*>(&lang[i])<<"\n";
     }
 
+    //Multiple ways to create strings
+    /*
+    string s0 = "just create it lol";
+    
+    string s1 = "initial string";
+    string s2(s1, 8, 3); //"str"
+    //With (x, y, z), uses string x, starts at index y, and goes z more characters
+
+    string s3 = ()"another character sequence", 12);
+    //Stops at index 12
+
+    string s4(10, 'x')
+    //String of 10 'x's
+
+    string s5(10, 42)
+    //String of 10 characters of ASCII value 42 (which is '*')
+    
+    */
+
     return 0;
 }
