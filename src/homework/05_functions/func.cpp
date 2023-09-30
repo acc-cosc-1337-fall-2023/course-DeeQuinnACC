d084478c1b9796c16e00c5911b7fa96f27bf7fdc
DeeQuinnACC <142953458+DeeQuinnACC@users.noreply.github.com>
2023-09-30 23:18:58 +0000

    homework 5 completed

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,62 @@
 //add include statements
+#include "func.h"
 
-//add function code here
+using std::string;
+
+//Get the percentage of characters that are "G" or "C" in a dna sequence
+double get_gc_content(const std::string& dna)
+{
+    int gc = 0;
+    for(int i = 0; i < dna.length(); i++)
+    {
+        if(dna[i] == 'G' || dna[i] == 'C')
+        {
+            gc++;
+        }
+    }
+
+    double gcContent = (double)gc / (double)dna.length();
+    return gcContent;
+}
+
+//Create a complement to a dna sequence
+std::string get_dna_complement(std::string dna)
+{
+    string reverse = reverse_string(dna);
+    string complement = "";
+    for(int i = 0; i < reverse.length(); i++)
+    {
+        switch(reverse[i])
+        {
+            case 'A':
+                complement += 'T';
+                break;
+            case 'T':
+                complement += 'A';
+                break;
+            case 'C':
+                complement += 'G';
+                break;
+            case 'G':
+                complement += 'C';
+                break;
+            default:
+                complement += 'X';
+                break;
+        }
+    }
+
+    return complement;
+}
+
+//Reverses any string pass into this function
+std::string reverse_string(std::string dna)
+{
+    string newDNA = "";
+    for(int i = dna.length() - 1; i >= 0; i--)
+    {
+        newDNA += dna[i];
+    }
+
+    return newDNA;
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,52 @@
+#include <iostream>
+#include "func.h"
+
+using std::cout;
+using std::cin;
+using std::string;
 
 int main() 
 {
+	char option = 0;
+	string dnaSequence = "XXXXXXXXXX";
+
+	do
+	{
+		//Display menu
+		cout<<"\n   Main Menu\n";
+		cout<<"1: Get GC Content\n";
+		cout<<"2: Get DNA Sequence\n";
+		cout<<"3: Exit Program\n";
+
+		cout<<"\nPlease select an option by entering the number to the left of it: ";
+		cin>>option;
+
+		if(option == '1') //GC Content
+		{
+			cout<<"\nPlease enter the DNA sequence: ";
+			cin>>dnaSequence;
+
+			double gcContent = get_gc_content(dnaSequence);
+			cout<<"The GC Content of this DNA sequence is "<<gcContent<<".\n";
+		}
+		else if(option == '2')//DNA Complement
+		{
+			cout<<"\nPlease enter the DNA sequence: ";
+			cin>>dnaSequence;
+
+			string complement = get_dna_complement(dnaSequence);
+			cout<<"The DNA complement to the inputed DNA sequence is:\n";
+			cout<<complement<<"\n";
+		}
+		else if(option == '3') //Display exit message
+		{
+			cout<<"\nExiting program...\n";
+		}
+		else //User inputted something unexpected, give error message
+		{
+			cout<<"\nERROR: Invalid selection. Please enter a valid option.\n";
+		}
+	} while (option != '3');
 
 	return 0;
 }
