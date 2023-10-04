009f23f23c77d87093de505c59c98a56c2b3dc63
DeeQuinnACC <142953458+DeeQuinnACC@users.noreply.github.com>
2023-10-04 00:59:59 +0000

    start learning OOP

--- src/examples/08_classes/01_bank/bank_account.h
+++ src/examples/08_classes/01_bank/bank_account.h
@@ -1,1 +1,13 @@
 //bank_account.h
+class BankAccount
+{
+    public:
+        BankAccount(int b) : balance(b) //constructor
+        {
+
+        } 
+        int get_balance(){ return balance; }
+
+    private:
+        int balance{0}; //{0} initializes the value
+};
