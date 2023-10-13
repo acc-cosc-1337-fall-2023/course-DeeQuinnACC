48e1afb882b61c32f9e3329151035c9d59dc02a0
DeeQuinnACC <142953458+DeeQuinnACC@users.noreply.github.com>
2023-10-13 00:08:42 +0000

    atm deposit and withdrawal

--- src/examples/08_classes/01_bank/atm.cpp
+++ src/examples/08_classes/01_bank/atm.cpp
@@ -2,8 +2,27 @@
 #include "atm.h"
 
 using std::cout;
+using std::cin;
 
 void ATM::display_balance()
 {
-    cout<<"ATM Balance: "<<account.get_balance();
+    cout<<"ATM Balance: "<<account.get_balance()<<"\n";
+}
+
+void ATM::make_deposit()
+{
+    int amount = 0;
+    cout<<"Enter amount to deposit: ";
+    cin>>amount;
+
+    account.deposit(amount);
+}
+
+void ATM::make_withdrawal()
+{
+    int amount = 0;
+    cout<<"Enter amount to withdraw: ";
+    cin>>amount;
+
+    account.withdraw(amount);
 }
