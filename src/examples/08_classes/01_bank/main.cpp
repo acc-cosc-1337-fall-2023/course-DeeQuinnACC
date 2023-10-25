a7c67c442f8d09d73c3714aa7da9f72ef5635c92
DeeQuinnACC <142953458+DeeQuinnACC@users.noreply.github.com>
2023-10-25 00:29:40 +0000

    virtual functions

--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -1,9 +1,10 @@
+#include <time.h> //help provide true randomness for rand()
+#include <iostream>
 #include "atm.h"
-#include<time.h> //help provide true randomness for rand()
-#include<iostream>
 #include "bank_account.h"
-#include "savings_account.h"
 #include "checking_account.h"
+#include "savings_account.h"
+
 
 using std::cout;
 
@@ -11,8 +12,16 @@ int main()
 {
 	srand(time(NULL)); //Make rand() truly sudo-random
 
-	BankAccount account;
-	SavingsAccount account1 (account.get_balance());
+	BankAccount* account;
 
-	cout<<"\n"<<account1.BankAccount::get_balance()<<" | "<<account1.get_balance()<<"\n";
+	SavingsAccount account1(500);
+	account = &account1;
+	cout<<account->get_balance()<<"\n";
+
+	CheckingAccount account2(500);
+	account = &account2;
+	cout<<account->get_balance()<<"\n";
+
+
+	//cout<<"\n"<<account1.BankAccount::get_balance()<<" | "<<account1.get_balance()<<"\n";
 }
