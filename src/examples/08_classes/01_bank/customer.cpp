9b5b7978d5e7a504e2166ff7fdec79d0d2536620
DeeQuinnACC <142953458+DeeQuinnACC@users.noreply.github.com>
2023-10-27 00:50:14 +0000

    customer class started

--- src/examples/08_classes/01_bank/customer.cpp
+++ src/examples/08_classes/01_bank/customer.cpp
@@ -1,1 +1,8 @@
 //customer.cpp
+#include "customer.h"
+
+Customer::Customer()
+{
+    accounts.push_back(&CheckingAccount());
+    accounts.push_back(&SavingsAccount());
+}
--- src/examples/08_classes/01_bank/customer.h
+++ src/examples/08_classes/01_bank/customer.h
@@ -1,1 +1,20 @@
 //customer.h
+#include "checking_account.h"
+#include "savings_account.h"
+#include <vector>
+
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+class Customer
+{
+public:
+    Customer();
+    //Customer(std::vector<BankAccount*> a) : accounts(a){}
+    BankAccount* get_account(int index){ return accounts[index]; }
+
+private:
+    std::vector<BankAccount*> accounts;
+};
+
+#endif
