ba1bd0b10a5ce02dc61683d0d2fdb9bbf6ae7bf7
DeeQuinnACC <142953458+DeeQuinnACC@users.noreply.github.com>
2023-12-06 01:05:52 +0000

    runaway recursion

--- src/examples/12_recursion/01_recursion/recursion.cpp
+++ src/examples/12_recursion/01_recursion/recursion.cpp
@@ -1,6 +1,10 @@
 #include "recursion.h"
 #include<iostream>
 //Write code for recursive display function
-
+void say_hello()
+{
+    std::cout<<"Hello\n";
+    say_hello();
+}
 
 //Write code for recursive factorial
--- src/examples/12_recursion/01_recursion/recursion.h
+++ src/examples/12_recursion/01_recursion/recursion.h
@@ -1,4 +1,3 @@
 //Write prototype for void function display with one int parameter
-
+void say_hello();
 //Write prototype for int value-return function with one int parameter
-
